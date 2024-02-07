5b2197f72326941ae3956b4b10f796dd3dacdcc6
tevinric <78318984+tevinric@users.noreply.github.com>
2024-02-07 21:47:12 +0200

    save

--- 9_PDF_chat_with_ChatGPT_Streamlit/ChatPDF_with_streamlit_app.py
+++ 9_PDF_chat_with_ChatGPT_Streamlit/ChatPDF_with_streamlit_app.py
@@ -11,9 +11,10 @@ from langchain.callbacks import get_openai_callback
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter # For splitting text into chunks
 import os
+import threading
 
 
-os.environ['OPENAI_API_KEY'] = "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"
+os.environ['OPENAI_API_KEY'] = "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"
 
 def main():
     st.header('Chat with your PDF')
@@ -26,10 +27,61 @@ def main():
     ''')
     
     # In streamlit, add a link by using [] to hold the text for the link followed by the () which holds the url
-    
     st.sidebar.write('You can write something here to make more information available')
 
-
+    #Upload PDF file - file path gets loaded to object
+    pdf = st.file_uploader("Upload your PDF file", type='pdf')
+    #pdf = r"C:\Users\tevin\OneDrive\Learning\GenAI_&_Langchain\GenAI_-_Langchain\8_PDF_chat_with_Langchain_openai\yolov7paper - Copy.pdf"
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)   # reads the information in the file
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+            
+        #Split text into multiple chunks
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1000,
+            chunk_overlap=200,
+            length_function=len)
+        
+        chunks = text_splitter.split_text(text=text)
+        
+        #Create the embeddings
+        # If the user uploads the same file again - to save on embeddings costs we can store the embeddings
+        store_name = pdf.name[:-4]
+        st.write(store_name)
+        
+        change_path = "C:\\Users\\tevin\\OneDrive\\Learning\\GenAI_&_Langchain\\GenAI_-_Langchain\\9_PDF_chat_with_ChatGPT_Streamlit"
+        os.chdir(change_path)
+        st.write(os.getcwd())
+        
+        if os.path.exists(f"{store_name}.pkl"):
+            with open(f"{store_name}.pickle", 'rb') as f:
+                VectorStore = pickle.load(f)
+            st.write("Embeddings loaded from the Disk")
+        else:
+            embeddings = OpenAIEmbeddings()
+            VectorStore = FAISS.from_texts(chunks, embeddings)
+            with open(f"{store_name}.pickle", 'wb') as f:
+                pickle.dump(VectorStore, f)
+            st.write('Embeddings Created')
+            
+        # Create an input for the user query
+        query = st.text_input("Ask Question from your PDF file")
+        #query = 'What is the paper about?'
+        if query:
+            docs= VectorStore.similarity_search(query=query, k=3)
+            llm = OpenAI()
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            
+            #Check cost for embeddings and extraction
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=query)
+                print(cb)
+            
+            st.write(response)
+    else:
+        st.write("Upload your PDF to start chatting with it!")
 
 if __name__ == '__main__':
     main()
